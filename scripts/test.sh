badb3a6554b9d969c7c51ec0940b39dbabecd8ba
bwireman <bwireman1@gmail.com>
2024-06-08 14:43:01 -0500

    prepare for 1.0.0

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,7 +5,9 @@ cd "$(dirname $0)/.."
 gleam update
 gleam build
 gleam format
-gleam test
+gleam test --target erlang
+gleam test --target javascript --runtime bun
+gleam test --target javascript --runtime nodejs
 rm -rf .go-over/
 gleam run -- --force
 gleam run -- --fake
