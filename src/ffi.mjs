2ca34506d97dfb240e141be6f85c58ff9c519824
bwireman <bwireman1@gmail.com>
2024-06-09 10:26:48 -0500

    prettier & eslint

--- src/ffi.mjs
+++ src/ffi.mjs
@@ -1,24 +1,21 @@
-import fetch from 'sync-fetch';
-import { parse as parse_yaml } from 'yaml'
-import { toList } from '../prelude.mjs'
+import fetch from "sync-fetch"
+import { parse as parse_yaml } from "yaml"
+import { toList } from "../prelude.mjs"
 
 export function do_fetch(path) {
-    return fetch(path, { headers: { "User-Agent": "gleam.go-over.fetch" } }).text()
+    return fetch(path, {
+        headers: { "User-Agent": "gleam.go-over.fetch" },
+    }).text()
 }
 
 export function parse_adv(content) {
-    const { id,
+    const {
+        id,
         package: pkg,
         severity,
         title,
         vulnerable_version_ranges,
     } = parse_yaml(content)
 
-    return [
-        id,
-        pkg,
-        severity,
-        title,
-        toList(vulnerable_version_ranges)
-    ]
+    return [id, pkg, severity, title, toList(vulnerable_version_ranges)]
 }
