5fe291e2ed2ce1a1371153a6808666a4653db9ff
bwireman <bwireman1@gmail.com>
2024-06-14 21:22:10 -0500

    2.0.0

--- scripts/test.sh
+++ scripts/test.sh
@@ -26,9 +26,12 @@ gleam test --target javascript --runtime deno
 rm -rf .go-over/
 echo -e "${GREEN}==> deno${NC}"
 gleam run --target javascript --runtime deno -- --force
+
 echo -e "${GREEN}==> bun${NC}"
 gleam run --target javascript --runtime bun
+
 echo -e "${GREEN}==> nodejs${NC}"
 gleam run --target javascript --runtime nodejs
+
 echo -e "${GREEN}==> erlang${NC}"
 gleam run -- --outdated
